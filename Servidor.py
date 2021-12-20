d3806b7a2b0f061f517a451c791d713c6528e99d
Edwin Pineda <61611378+edwinpineda96@users.noreply.github.com>
2021-12-20 14:18:35 -0600

    ....

---
+++ Servidor.py
@@ -0,0 +1,11 @@
+import ee
+import geemap
+import pandas as pd
+import datetime
+from datetime import timedelta
+
+def mensaje():
+    print ("puntos de fuejo")
+
+if __name__ == '__main__':
+    mensaje()
